401e93ecf6bb4119ef19606f1ea07c598cc88b4c
kaiostavares <kaiosandek@outlook.com>
2023-05-13 19:15:39 -0300

    questão 1 v.1

---
+++ q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  float pi = 3.14159;
+  float raio, area;
+  printf("Digite o raio da circunferencia\n");
+  scanf("%f", &raio);
+  area = pi * (raio*raio);
+  printf("A area da circuferncia vai ser de %0.3f", area);
+  return 0;
+}
