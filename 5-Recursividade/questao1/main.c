19ba2a52501b718198e938b08b895bef538dcb87
Kaio Tavares IF <129338819+IfKaioTavares@users.noreply.github.com>
2023-06-28 23:16:11 +0000

    incompleto

---
+++ 5-Recursividade/questao1/main.c
@@ -0,0 +1,21 @@
+//Escreva uma função recursiva que calcule a soma dos primeiros n cubos: S= 1^3 + 2^3 + n^3
+
+#include <stdio.h>
+#include <math.h>
+
+int somatorioDeElevadoA3(int n);
+
+void main(){
+
+}
+
+
+int somatorioDeElevadoA3(int n){
+    int sum = 0;
+
+    if(n<=1){
+        return(1);
+    } else{
+        sum
+    }
+}
